e7fade560fc6642df750b161f710f00df9b5e265
mykegin <mvginghina@gmail.com>
2024-03-06 22:28:25 +0200

    incerc a doua problema

---
+++ lab2/Seminar2Ex2/Seminar2Ex2/main.cpp
@@ -0,0 +1,16 @@
+#include "CarnetElev.h"
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	CarnetElev myClass;
+	myClass.SetNume("Ginghina");
+	printf("numele: %s \n", myClass.GetNume);
+
+
+	/*printf("val x: %d\n", myClass.Getx());
+	myClass.Setx(30);
+	printf("val x: %d\n", myClass.Getx());*/
+
+}
