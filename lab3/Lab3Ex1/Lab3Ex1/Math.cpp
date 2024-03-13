86a5555affc3f16012198f1f6ec74a55355a255d
mykegin <mvginghina@gmail.com>
2024-03-13 18:48:17 +0200

    am rezolvat problema 1

--- lab3/Lab3Ex1/Lab3Ex1/Math.cpp
+++ lab3/Lab3Ex1/Lab3Ex1/Math.cpp
@@ -78,12 +78,18 @@ char* Math::Add(const char* sir1, const char* sir2)
 		return nullptr;
 
 	int lungime;
-	int nr, cifra, surplus;
+	int nr, cifra, surplus=0;
 	
-	if (strlen(sir1) > strlen(sir2))
+	if (strlen(sir1) >= strlen(sir2))
 	{
 		lungime = strlen(sir1) + 1;
-		int diferenta = strlen(sir1) - strlen(sir2);
+		char* sirr1 = new char[lungime];
+		sirr1[0] = '0';
+		for (int i = 1; i < lungime; i++)
+			sirr1[i] = sir1[i - 1];
+		sirr1[lungime] = '\0';
+		
+		int diferenta = strlen(sir1) - strlen(sir2)+1;
 		char* nivelare= new char [diferenta];
 		for (int i = 0; i < diferenta; i++)
 			nivelare[i] = '0';
@@ -92,35 +98,98 @@ char* Math::Add(const char* sir1, const char* sir2)
 
 		char* resultat = new char[lungime];
 
+		surplus = 0;
+		for (int i = lungime - 1; i >= 0; i--)
+			resultat[i] = '0';
+
 		for (int i = lungime - 1; i >= 0; i--)
 		{
-			int j = i - 1;
-			nr = 0;
-			cifra = 0;
-			surplus = 0;
-			resultat[i] = resultat[i - 1] = '0';
-			nr = ((sir1[j] - '0') + (nivelare[j] - '0'));
+			nr = ((sirr1[i] - '0') + (nivelare[i] - '0') + (resultat[i]-'0'));
 			cifra = nr % 10;
-			resultat[i] = resultat[i] + cifra;
-			printf("cifra este: %c\n", resultat[i]);
+			if (nr > 9)
+				surplus = 1;
 			if (surplus)
 			{
-				cifra = nr % 10;
-				resultat[i] = resultat[i] + nr / 10 + '0';
+				resultat[i-1] = '1';
 			}
-			if (nr / 10 > 0)
-				surplus = 1;
+			surplus = 0;
+
+			printf("\n");
+			printf("cifra char este: %c\n", resultat[i]);
+			resultat[i] = '0';
+			resultat[i] = resultat[i] + cifra;
+
+			printf("numarul este: %d\n", nr);
+			printf("cifra este: %d\n", cifra);
+			printf("cifra char actuala este: %c\n", resultat[i]);
+			printf("cifra char la urmatorul este: %c\n", resultat[i-1]);
+			printf("\n");
+
+			if (i == 0 && resultat[0] == '0')
+			{
+				for (int j = 0; j <= lungime - 1; j++)
+					resultat[j] = resultat[j + 1];
+				lungime--;
+			}
+
 			resultat[lungime] = '\0';
 		}
 		return resultat;
 	}
-	/*else
+	else
 	{
 		lungime = strlen(sir2) + 1;
-		int diferenta = strlen(sir1) - strlen(sir2);
+		char* sirr2 = new char[lungime];
+		sirr2[0] = '0';
+		for (int i = 1; i < lungime; i++)
+			sirr2[i] = sir2[i - 1];
+		sirr2[lungime] = '\0';
+
+		int diferenta = strlen(sir2) - strlen(sir1) + 1;
 		char* nivelare = new char[diferenta];
 		for (int i = 0; i < diferenta; i++)
 			nivelare[i] = '0';
-		printf("diferenta este: %d\n", diferenta);
-	}*/
+		nivelare[diferenta] = '\0';
+		strcat(nivelare, sir1);
+
+		char* resultat = new char[lungime];
+
+		surplus = 0;
+		for (int i = lungime - 1; i >= 0; i--)
+			resultat[i] = '0';
+
+		for (int i = lungime - 1; i >= 0; i--)
+		{
+			nr = ((sirr2[i] - '0') + (nivelare[i] - '0') + (resultat[i] - '0'));
+			cifra = nr % 10;
+			if (nr > 9)
+				surplus = 1;
+			if (surplus)
+			{
+				resultat[i - 1] = '1';
+			}
+			surplus = 0;
+
+			printf("\n");
+			printf("cifra char este: %c\n", resultat[i]);
+			resultat[i] = '0';
+			resultat[i] = resultat[i] + cifra;
+
+			printf("numarul este: %d\n", nr);
+			printf("cifra este: %d\n", cifra);
+			printf("cifra char actuala este: %c\n", resultat[i]);
+			printf("cifra char la urmatorul este: %c\n", resultat[i - 1]);
+			printf("\n");
+
+			if (i == 0 && resultat[0] == '0')
+			{
+				for (int j = 0; j <= lungime - 1; j++)
+					resultat[j] = resultat[j + 1];
+				lungime--;
+			}
+
+			resultat[lungime] = '\0';
+		}
+		return resultat;
+	}
 }
